29ff1ca1c1bc5a9b893eaf9d1badc094e85783de
Jeff Stokes <jeffecu88@gmail.com>
2015-02-08 09:47:10 -0500

    Add implementation for candy machine and tests

--- src/main/scala/funprog/state/Machine.scala
+++ src/main/scala/funprog/state/Machine.scala
@@ -8,5 +8,17 @@ case object Coin extends Input
 case object Turn extends Input
 
 object Candy {
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs.map(input => modify(onInput(input))))
+    s <- get
+  } yield (s.coins, s.candies)
+
+  def onInput(input: Input): Machine => Machine =
+    machine => (machine, input) match {
+      case (Machine(_, candy, coin), _) if candy <= 0 => machine
+      case (Machine(true, candy, coin), Coin) => Machine(false, candy, coin + 1)
+      case (Machine(false, candy, coin), Turn) => Machine(true, candy - 1, coin)
+      case (Machine(true, candy, coin), Turn) => machine
+      case (Machine(false, candy, coin), Coin) => machine
+    }
 }
--- src/test/scala/funprog/state/MachineTest.scala
+++ src/test/scala/funprog/state/MachineTest.scala
@@ -3,6 +3,7 @@ package funprog.state
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import org.scalacheck.Gen
+import org.scalacheck.Gen._
 import org.scalacheck.Prop
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary._
@@ -11,15 +12,40 @@ object CandyTest extends Specification with ScalaCheck {
   import Candy._
 
   implicit def arbMachine: Arbitrary[Machine] = Arbitrary {
-    for (candies <- arbitrary[Int]; coins <- arbitrary[Int]; locked <- arbitrary[Boolean])
+    for (candies <- posNum[Int]; coins <- posNum[Int]; locked <- arbitrary[Boolean])
       yield Machine(locked, candies, coins)
   }
 
-  val withCandy = arbitrary[Machine] suchThat (_.candies > 0)
+  val arbM = arbitrary[Machine]
+  val locked = arbM map (m => m.copy(locked = true))
+  val unlocked = arbM map (m => m.copy(locked = false))
+  val noCandy = arbM map (m => m.copy(candies = 0))
+  val withCandy: Machine => Boolean = _.candies > 0
+  val arbInput = Gen.listOf(Gen.oneOf(Coin, Turn))
 
   "inserting a coin into a locked machine will cause it to unlock, if there is any candy left" !
-    Prop.forAll(withCandy) { (m: Machine) =>
-      val result = simulateMachine(List(Coin)).run(m)._2
-      result.locked must beFalse
-    }.pendingUntilFixed
+    Prop.forAll(locked filter withCandy) { m: Machine =>
+      simulateMachine(List(Coin)).run(m)._2.locked must beFalse
+    }
+
+  "turning the knob on an unlocked machine will cause it to dispense candy and become locked" !
+    Prop.forAll(unlocked) { m: Machine =>
+      val result = simulateMachine(List(Turn)).run(m)._2
+      result.locked == true && result.candies == m.candies - 1
+    }
+
+  "turning the knob on a locked machine does nothing" ! Prop.forAll(locked) { m: Machine =>
+    simulateMachine(List(Turn)).run(m)._2 == m
+  }
+
+
+  "inserting a coin into an unlocked machine does nothing" ! Prop.forAll(unlocked) { m: Machine =>
+    val result = simulateMachine(List(Coin)).run(m)._2
+    result == m
+  }
+
+  "a machine that is out of candy ignores all inputs" ! Prop.forAll(noCandy, arbInput) {
+    (m: Machine, inputs: List[Input]) =>
+      simulateMachine(inputs).run(m)._2 == m
+  }
 }
