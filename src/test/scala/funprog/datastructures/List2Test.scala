5d2bd800540778d49a8b0256b1633138ef1adee5
Jeff Stokes <jeffecu88@gmail.com>
2015-02-10 13:15:23 -0500

    Pull the lists tests from master branch

--- src/test/scala/funprog/datastructures/List2Test.scala
+++ src/test/scala/funprog/datastructures/List2Test.scala
@@ -1,4 +1,3 @@
-
 package funprog.datastructures
 
 import org.scalacheck.Gen
@@ -17,76 +16,173 @@ object ListTest extends Specification with ScalaCheck {
   def plus(a: Int, b: Int) = a + b
   def minus(a: Int, b: Int) = a - b
 
-  "apply should create a list" in {
-    List2[Int]() mustEqual Nil
-    List2(1) mustEqual Cons(1, Nil)
-    List2(1, 2, 3) mustEqual Cons(1, Cons(2, Cons(3, Nil)))
+  implicit def arbList: Arbitrary[List2[Int]] = Arbitrary(
+    arbitrary[List[Int]].map(l => List2(l: _*))
+  )
+  def nonEmptyList = arbitrary[List2[Int]] suchThat (l => len(l) > 0)
+
+  "apply" in {
+    "apply should create a list" in {
+      List2[Int]() mustEqual Nil
+      List2(1) mustEqual Cons(1, Nil)
+      List2(1, 2, 3) mustEqual Cons(1, Cons(2, Cons(3, Nil)))
+    }
   }
 
-  "len of empty list is 0" in {
-    len(empty) mustEqual 0
+  "len" in {
+    "len of empty list is 0" in {
+      len(empty) mustEqual 0
+    }
+
+    "len of a one element list is 1" in {
+      len(List2(1)) mustEqual 1
+    }
+
+    "length of a n element n is n" ! prop { as: List[Int] =>
+      as.length == len(List2(as: _*))
+    }
+
+    "the length of the tail is 1 less than the length of the list" ! forAll(nonEmptyList) { as: List2[Int] =>
+      len(as) == 1 + len(tail(as))
+    }
   }
 
-  "len of a one element list is 1" in {
-    len(List2(1)) mustEqual 1
+  "tail" in {
+    "tail on an empty lists results in an exception" in {
+      tail(List2()) must throwA[UnsupportedOperationException]
+    }
+
+    "the tail of a list is the list without it's head" ! prop { as: List2[Int] =>
+      as match {
+        case Cons(h, t) => tail(as) == t
+        case Nil => true
+      }
+    }
   }
 
-  "tail on an empty lists results in an exception" in {
-    tail(List2()) must throwA[UnsupportedOperationException]
+  "setHead" in {
+    "set head changes the head of a list without changing the tail" in {
+      setHead(empty, 3) must throwA[UnsupportedOperationException]
+      setHead(List2(1), 3) mustEqual List2(3)
+      setHead(List2(2, 2, 3), 1) mustEqual List2(1, 2, 3)
+    }
+
+    "setHead can change the head without changing the tail" ! forAll(nonEmptyList) { as: List2[Int] =>
+      setHead(as, 3) match {
+        case Cons(h, t) => h == 3 && t == tail(as)
+        case _ => false
+      }
+    }
   }
 
-  "set head changes the head of a list without changing the tail" in {
-    setHead(empty, 3) must throwA[UnsupportedOperationException]
-    setHead(List2(1), 3) mustEqual List2(3)
-    setHead(List2(2, 2, 3), 1) mustEqual List2(1, 2, 3)
+  "drop" in {
+    "drop removes items from the front of a list" in {
+      drop(List2(), 100) mustEqual List2()
+      drop(List2(1, 2, 3), 2) mustEqual List2(3)
+    }
+
+    "drop(as, n) should result in a list that is shorter when n > 0" ! prop { (n: Int, as: List2[Int]) =>
+      drop(as, n) match {
+        case Nil => n >= len(as) || as == Nil
+        case l if n > 0 => len(as) == n + len(l)
+        case l if n <= 0 => l == as
+      }
+    }
+
+    "drop should remove from the start of the list" ! forAll(nonEmptyList) { as: List2[Int] =>
+      drop(as, 1) match {
+        case Nil => len(as) == 1
+        case l => l == tail(as)
+      }
+    }
   }
 
-  "drop removes items from the front of a list" in {
-    drop(List2(), 100) mustEqual List2()
-    drop(List2(1, 2, 3), 2) mustEqual List2(3)
+  "dropWhile" in {
+    "dropWhile removes elements from the head of the list using a predicate function until it returns false" in {
+      dropWhile(empty, isEven) mustEqual empty
+      dropWhile(List2(1, 3, 5), isEven) mustEqual List2(1, 3, 5)
+      dropWhile(List2(2, 4, 6), isEven) mustEqual empty
+      dropWhile(List2(2, 4, 6, 7), isEven) mustEqual List2(7)
+    }
   }
 
-  "init drops the last element in a list" in {
-    init(List2(1)) mustEqual List2()
-    init(List2(1, 2, 3)) mustEqual List2(1, 2)
+  "init" in {
+    "init drops the last element in a list" in {
+      init(List2(1)) mustEqual List2()
+      init(List2(1, 2, 3)) mustEqual List2(1, 2)
+    }
   }
 
-  "map takes a List2[A] and a function from A => B and returns a List2[B]" in {
-    List2.map(empty)(times2) mustEqual empty
-    List2.map(List2(1, 2, 3))(times2) mustEqual List2(2, 4, 6)
+  "map" in {
+    "map takes a List2[A] and a function from A => B and returns a List2[B]" in {
+      List2.map(empty)(times2) mustEqual empty
+      List2.map(List2(1, 2, 3))(times2) mustEqual List2(2, 4, 6)
+    }
   }
 
-  "flatMap is like map but must also wrap" in {
-    flatMap(empty)(i => List2(empty)) mustEqual empty
-    flatMap(List2(1, 2, 3))(i => List2(i, i)) mustEqual List2(1, 1, 2, 2, 3, 3)
+  "sum" in {
+    "sum adds all elements of a list together" in {
+      sum(empty) mustEqual 0
+      sum(List2(1)) mustEqual 1
+      sum(List2(1, 2, 3)) mustEqual 6
+    }
   }
 
-  "append takes two lists and creates a single list of the combined elements l1 + l2" in {
-    append(empty, empty) mustEqual empty
-    append(List2(1, 2, 3), List2(4, 5, 6)) mustEqual List2(1, 2, 3, 4, 5, 6)
+  "product" in {
+    "take the product of a list of doubles" in {
+      product(List2[Double]()) mustEqual 1
+      product(List2(1.0)) mustEqual 1.0
+      product(List2(2.0, 2.5)) mustEqual 5.0
+    }
   }
 
-  "filter takess a predicate function only keeps the elements that return true" in {
-    filter(empty)(isEven) mustEqual empty
-    filter(List2(1, 2, 3))(isEven) mustEqual List2(2)
-    filter(List2(1, 3, 5))(isEven) mustEqual empty
-    filter(List2(2, 4, 6))(isEven) mustEqual List2(2, 4, 6)
+  "reverse" in {
+    "reverse flips the order of a list" in {
+      reverse(empty) mustEqual empty
+      reverse(List2(1, 2, 3)) mustEqual List2(3, 2, 1)
+    }
   }
 
-  "dropWhile removes elements from the head of the list using a predicate function until it returns false" in {
-    dropWhile(empty, isEven) mustEqual empty
-    dropWhile(List2(1, 3, 5), isEven) mustEqual List2(1, 3, 5)
-    dropWhile(List2(2, 4, 6), isEven) mustEqual empty
-    dropWhile(List2(2, 4, 6, 7), isEven) mustEqual List2(7)
+  "append" in {
+    "append takes two lists and creates a single list of the combined elements l1 + l2" in {
+      append(empty, empty) mustEqual empty
+      append(List2(1, 2, 3), List2(4, 5, 6)) mustEqual List2(1, 2, 3, 4, 5, 6)
+    }
   }
 
-  "concat 'flattens' a list of lists by appending them all into a single list" in {
-    concat(List2(List2(empty), List2(empty), List2(empty))) mustEqual List2(empty, empty, empty)
-    concat(List2(List2(1), List2(2), List2(3))) mustEqual List2(1, 2, 3)
+  "filter" in {
+    "filter takess a predicate function only keeps the elements that return true" in {
+      filter(empty)(isEven) mustEqual empty
+      filter(List2(1, 2, 3))(isEven) mustEqual List2(2)
+      filter(List2(1, 3, 5))(isEven) mustEqual empty
+      filter(List2(2, 4, 6))(isEven) mustEqual List2(2, 4, 6)
+    }
+  }
+
+  "concat" in {
+    "concat 'flattens' a list of lists by appending them all into a single list" in {
+      concat(List2(List2(empty), List2(empty), List2(empty))) mustEqual List2(empty, empty, empty)
+      concat(List2(List2(1), List2(2), List2(3))) mustEqual List2(1, 2, 3)
+    }
+  }
+
+  "flatMap" in {
+    "flatMap is like map but must also wrap" in {
+      flatMap(empty)(i => List2(empty)) mustEqual empty
+      flatMap(List2(1, 2, 3))(i => List2(i, i)) mustEqual List2(1, 1, 2, 2, 3, 3)
+    }
+  }
+
+  "zipWith" in {
+    "zipWith combines two lists with a joining function" in {
+      zipWith(empty, empty)(plus) mustEqual empty
+      zipWith(List2(1), List2(1))(plus) mustEqual List2(2)
+      zipWith(List2(1, 2, 3), List2(1, 2))(plus) mustEqual List2(2, 4)
+      zipWith(List2(1, 2), List2(1, 2, 3))(plus) mustEqual List2(2, 4)
+    }
   }
 
   "folding" in {
-
     "foldRight takes an initial value, and a merging function, and returns a final result" in {
       foldRight(empty, 0)(plus) mustEqual 0
       foldRight(List2(1, 2, 3), 0)(plus) mustEqual (1 + (2 + (3 + 0)))
@@ -106,84 +202,17 @@ object ListTest extends Specification with ScalaCheck {
     }
   }
 
-  "reverse flips the order of a list" in {
-    reverse(empty) mustEqual empty
-    reverse(List2(1, 2, 3)) mustEqual List2(3, 2, 1)
-  }
-
-  "sum adds all elements of a list together" in {
-    sum(empty) mustEqual 0
-    sum(List2(1)) mustEqual 1
-    sum(List2(1, 2, 3)) mustEqual 6
-  }
-
-  "take the product of a list of doubles" in {
-    product(List2[Double]()) mustEqual 1
-    product(List2(1.0)) mustEqual 1.0
-    product(List2(2.0, 2.5)) mustEqual 5.0
-  }
-
-  "zipWith combines two lists with a joining function" in {
-    zipWith(empty, empty)(plus) mustEqual empty
-    zipWith(List2(1), List2(1))(plus) mustEqual List2(2)
-    zipWith(List2(1, 2, 3), List2(1, 2))(plus) mustEqual List2(2, 4)
-    zipWith(List2(1, 2), List2(1, 2, 3))(plus) mustEqual List2(2, 4)
-  }
-
   "hasSubSequence can identity sub sequences" in {
     "empty never has subsequence" ! prop { as: List2[Int] =>
       hasSubsequence(empty, as) must beFalse
     }
+
     "empty always is a subsequence of non-empty lists" ! forAll(nonEmptyList) { as: List2[Int] =>
       hasSubsequence(as, empty) must beTrue
     }
+
     "tail is always a subsequence" ! forAll(nonEmptyList) { as: List2[Int] =>
       hasSubsequence(as, tail(as)) must beTrue
-    }
-  }
-
-  implicit def arbList: Arbitrary[List2[Int]] = Arbitrary(
-    arbitrary[List[Int]].map(l => List2(l: _*))
-  )
-  def nonEmptyList = arbitrary[List2[Int]] suchThat (l => len(l) > 0)
-
-  "obey properties" in {
-    "length of a n element n is n" ! prop { as: List[Int] =>
-      as.length == len(List2(as: _*))
-    }
-
-    "the length of the tail is 1 less than the length of the list" !
-      forAll(nonEmptyList) { as: List2[Int] =>
-        len(as) == 1 + len(tail(as))
-      }
-
-    "the tail of a list is the list without it's head" ! prop { as: List2[Int] =>
-      as match {
-        case Cons(h, t) => tail(as) == t
-        case Nil => true
-      }
-    }
-
-    "setHead can change the head without changing the tail" ! forAll(nonEmptyList) { as: List2[Int] =>
-      setHead(as, 3) match {
-        case Cons(h, t) => h == 3 && t == tail(as)
-        case _ => false
-      }
-    }
-
-    "drop(as, n) should result in a list that is shorter when n > 0" ! prop { (n: Int, as: List2[Int]) =>
-      drop(as, n) match {
-        case Nil => n >= len(as) || as == Nil
-        case l if n > 0 => len(as) == n + len(l)
-        case l if n <= 0 => l == as
-      }
-    }
-
-    "drop should remove from the start of the list" ! forAll(nonEmptyList) { as: List2[Int] =>
-      drop(as, 1) match {
-        case Nil => len(as) == 1
-        case l => l == tail(as)
-      }
     }
   }
 }
