6ade6fdec6e6bd8b2458e2f8768ff11768eeb4d0
Jeff Stokes <jeffecu88@gmail.com>
2015-02-09 22:19:12 -0500

    Initial pass at Par impl

---
+++ src/main/scala/funprog/parallelism/Par.scala
@@ -0,0 +1,40 @@
+package funprog.parallelism
+
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
+
+  def asyncF[A,B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  def map[A,B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit(()))((a, _) => f(a))
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight(unit(List[A]()))((x, y) => map2(x, y)(_ :: _))
+}
